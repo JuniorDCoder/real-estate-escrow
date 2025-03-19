069090fccd1f830f2f434e4e58c827c7c0e10d57
JuniorDCoder <foryoungjuniorngu@gmail.com>
2025-03-19 12:32:20 +0100

    feat: Completed RealEstate contract Deployed it locally using hardhat successfully Ran all tests Now to start working on the react frontend

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -16,10 +16,28 @@ contract Escrow {
         _;
     }
 
+    modifier onlyBuyer(uint256 _nftID) {
+        require(
+            msg.sender == buyer[_nftID],
+            "Only buyer can call this function"
+        );
+        _;
+    }
+
+    modifier onlyInspector() {
+        require(
+            msg.sender == inspector,
+            "Only inspector can call this function"
+        );
+        _;
+    }
+
     mapping(uint256 => bool) public isListed;
     mapping(uint256 => uint256) public purchasePrice;
     mapping(uint256 => uint256) public escrowAmount;
     mapping(uint256 => address) public buyer;
+    mapping(uint256 => bool) public inspectionPassed;
+    mapping(uint256 => mapping(address => bool)) public approval;
 
     constructor(
         address _nftAddress,
@@ -46,5 +64,65 @@ contract Escrow {
         purchasePrice[_nftID] = _purchasePrice;
         escrowAmount[_nftID] = _escrowAmount;
         buyer[_nftID] = _buyer;
+    }
+
+    // Put under Contract( only buyer- payable escrow)
+    function depositEarnest(uint256 _nftID) public payable onlyBuyer(_nftID) {
+        require(msg.value >= escrowAmount[_nftID], "Invalid escrow amount");
+    }
+
+    function updateInspectionStatus(
+        uint256 _nftID,
+        bool _status
+    ) public onlyInspector {
+        inspectionPassed[_nftID] = _status;
+    }
+
+    function approveSale(uint256 _nftID) public {
+        approval[_nftID][msg.sender] = true;
+    }
+
+    function finalizeSale(uint256 _nftID) public {
+        require(inspectionPassed[_nftID] == true, "Inspection not passed yet");
+        require(
+            approval[_nftID][buyer[_nftID]] == true,
+            "Buyer has not approved the sale"
+        );
+        require(
+            approval[_nftID][seller] == true,
+            "Seller has not approved the sale"
+        );
+        require(
+            approval[_nftID][lender] == true,
+            "Lender has not approved the sale"
+        );
+
+        require(
+            address(this).balance >= purchasePrice[_nftID],
+            "Insufficient balance"
+        );
+
+        isListed[_nftID] = false;
+
+        (bool success, ) = payable(seller).call{value: address(this).balance}(
+            ""
+        );
+        require(success, "Transfer failed");
+
+        IERC721(nftAddress).transferFrom(address(this), buyer[_nftID], _nftID);
+    }
+
+    function cancelSale(uint256 _nftID) public {
+        if (inspectionPassed[_nftID] == false) {
+            payable(buyer[_nftID]).transfer(address(this).balance);
+        } else {
+            payable(seller).transfer(address(this).balance);
+        }
+    }
+
+    receive() external payable {}
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
     }
 }
--- test/Escrow.js
+++ test/Escrow.js
@@ -80,4 +80,64 @@ describe('Escrow', () => {
             expect(result).to.equal(tokens(5));
         });
     });
+
+    describe('Deposit', async() => {
+        it('updates the escrow balance', async() => {
+            const transaction = await escrow.connect(buyer).depositEarnest(1, { value: tokens(5) });
+            await transaction.wait();
+
+            const result = await escrow.getBalance();
+            expect(result).to.equal(tokens(5));
+        });
+    });
+
+    describe('Inspection', async() => {
+        it('updates inspection status', async() => {
+            const transaction = await escrow.connect(inspector).updateInspectionStatus(1, true);
+            await transaction.wait();
+
+            const result = await escrow.inspectionPassed(1);
+            expect(result).to.equal(true);
+        });
+    });
+
+    describe('Approval', async() => {
+        it('updates approval status', async() => {
+            let transaction = await escrow.connect(buyer).approveSale(1);
+            await transaction.wait();
+            
+            transaction = await escrow.connect(seller).approveSale(1);
+            await transaction.wait();
+
+            transaction = await escrow.connect(lender).approveSale(1);
+            await transaction.wait();
+
+            expect(await escrow.approval(1, buyer.address)).to.equal(true);
+            expect(await escrow.approval(1, seller.address)).to.equal(true);
+            expect(await escrow.approval(1, lender.address)).to.equal(true);
+        });
+    });
+
+    describe('Sale', async() => {
+        beforeEach(async() => {
+            await escrow.connect(buyer).depositEarnest(1, { value: tokens(5) });
+            await escrow.connect(inspector).updateInspectionStatus(1, true);
+            await escrow.connect(buyer).approveSale(1);
+            await escrow.connect(seller).approveSale(1);
+            await escrow.connect(lender).approveSale(1);
+
+            await lender.sendTransaction({ to: escrow.address, value: tokens(5) });
+
+            await escrow.connect(seller).finalizeSale(1);
+        });
+
+        it('updates the balance of the seller', async() => {
+            const result = await escrow.getBalance();
+            expect(result).to.equal(tokens(0));
+        });
+
+        it('updates the ownership of the NFT', async() => {
+            expect(await realEstate.ownerOf(1)).to.equal(buyer.address);
+        });
+    });
 });
